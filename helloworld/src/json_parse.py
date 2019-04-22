0f9e68918c4a9a8347944540e3840534b663b8a7
zhongxj <zhongxj@chinawiserv.com>
2019-04-22 16:21:28 +0800

    json 解析

---
+++ helloworld/src/json_parse.py
@@ -0,0 +1,18 @@
+import json
+
+if __name__ == '__main__':
+    pass
+# with open("../resource/test.json","r+") as f:
+#     str = ""
+#     for line in f.readlines():
+#         str += line
+#     # 将读取的json字符串转为字典
+#     j = json.loads(str)
+#     print(j)
+
+# with open("../resource/test.json","r+") as f:
+#    str ={"name":"tom","age":18}
+#    将字典对象转为json字符串并写入f文件中
+#    json.dump(str,f,indent=4)
+#    将字典对象转为json字符串不写入f文件中
+#    json.dumps(str,indent=4)
